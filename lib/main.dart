9d5eafc801496162912f4abccbfc943bfb1541e9
sk-weepet <67887045+sk-weepet@users.noreply.github.com>
2020-07-09 01:09:10 +0900

    Stateful & setState w/method

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,18 +16,27 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    var leftDiceNumber = 5;
+  _DicePageState createState() => _DicePageState();
+}
 
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 5;
+  var rightDiceNumber = 1;
+  void change() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
+  Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Left Button got pressed.');
+                setState(change);
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -34,9 +44,10 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: FlatButton(
               onPressed: () {
+                setState(change);
                 print('Right button got pressed.');
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
